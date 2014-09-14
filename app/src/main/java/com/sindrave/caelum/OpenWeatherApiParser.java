2e39d3b42ec4d627143e89ad777107f64ab66b33
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2014-09-14 22:14:28 +0200

    Added parsing code for open weather api JSON data

---
+++ app/src/main/java/com/sindrave/caelum/OpenWeatherApiParser.java
@@ -0,0 +1,46 @@
+package com.sindrave.caelum;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Yanik on 14/09/2014.
+ */
+public class OpenWeatherApiParser {
+
+    public static Forecast parseForecastFromJson(JSONObject data) {
+        try {
+            Coords location = getLocationFromJson(data);
+            SunCycle sunCycle = getSunCycleFromJson(data);
+            JSONObject mainJsonObject = data.getJSONObject("main");
+            Weather weather = getWeatherFromJson(data, mainJsonObject);
+            TemperatureForecast tempForecast = getTemperatureForecastFromJson(mainJsonObject);
+            long date = data.getLong("dt");
+            return new Forecast(location, sunCycle, weather, tempForecast, date);
+        } catch (JSONException e) {
+            Log.e(OpenWeatherApiParser.class.getName(), "Error occurred while parsing JSON", e);
+            return null;
+        }
+    }
+
+    private static TemperatureForecast getTemperatureForecastFromJson(JSONObject mainJsonObject) throws JSONException {
+        return new TemperatureForecast((float)mainJsonObject.getDouble("temp"), (float)mainJsonObject.getDouble("temp_min"), (float)mainJsonObject.getDouble("temp_max"));
+    }
+
+    private static Weather getWeatherFromJson(JSONObject data, JSONObject mainJsonObject) throws JSONException {
+        JSONObject firstWeatherJsonObject = (JSONObject) data.getJSONArray("weather").get(0);
+        return new Weather(mainJsonObject.getInt("pressure"), mainJsonObject.getInt("humidity"), firstWeatherJsonObject.getString("description"),firstWeatherJsonObject.getString("description"));
+    }
+
+    private static SunCycle getSunCycleFromJson(JSONObject data) throws JSONException {
+        JSONObject sysObject = data.getJSONObject("sys");
+        return new SunCycle(sysObject.getLong("sunrise"), sysObject.getLong("sunset"));
+    }
+
+    private static Coords getLocationFromJson(JSONObject data) throws JSONException {
+        JSONObject jsonCoords = data.getJSONObject("coord");
+        return new Coords((float)jsonCoords.getDouble("lon"), (float)jsonCoords.getDouble("lat"));
+    }
+}
--- app/src/main/java/com/sindrave/caelum/WeatherApi.java
+++ app/src/main/java/com/sindrave/caelum/WeatherApi.java
@@ -2,18 +2,62 @@ package com.sindrave.caelum;
 
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Yanik on 15/08/2014.
  */
 public class WeatherApi {
-    private final static String OPEN_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}";
+    private final static String OPEN_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    public static final int HTTP_OK_RESPONSE_CODE = 200;
 
-    public WeatherApi() {
+    private JSONObject getForecastDataForCity(String city) {
+        String formattedUrlString = String.format(OPEN_WEATHER_API_URL, city);
+        StringBuilder json = new StringBuilder();
+        try {
+            BufferedReader reader = getReaderFromUrl(formattedUrlString);
+            String bufferedJson;
+            while ((bufferedJson = reader.readLine()) != null) {
+                json.append(bufferedJson).append("\n");
+            }
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+
+            if (data.getInt("cod") != HTTP_OK_RESPONSE_CODE) {
+                Log.w(WeatherApi.class.getName(), "The service returned a non-OK HTTP response");
+                return null;
+            }
+            return data;
+        } catch (MalformedURLException e) {
+            Log.e(WeatherApi.class.getName(), "Malformed URL: " + formattedUrlString, e);
+            return null;
+        } catch (IOException e) {
+            Log.e(WeatherApi.class.getName(), "unable to open connection to URL: " + formattedUrlString, e);
+            return null;
+        } catch (JSONException e) {
+            Log.e(WeatherApi.class.getName(), "Error parsing json: " + json.toString());
+            return null;
+        }
+    }
+
+    private BufferedReader getReaderFromUrl(String formattedUrlString) throws IOException {
+        URL apiUrl = new URL(formattedUrlString);
+        HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+        return new BufferedReader(new InputStreamReader(connection.getInputStream()));
     }
 
     public Forecast getForecast(String city){
         try {
-            return null;
+            JSONObject forecastDataForCityJson = getForecastDataForCity(city);
+            return OpenWeatherApiParser.parseForecastFromJson(forecastDataForCityJson);
         } catch (Exception e) {
             Log.e(WeatherApi.class.getName(), "Error parsing the json response", e);
             return null;
