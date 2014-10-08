533bc02e30a3f1d91fcfefdf53df8fcfd443550b
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2014-10-08 22:00:41 +0200

    Changed WeatherApi from interface to abstract class. Pulled caching code out of implementation and into the abstract class.

--- app/src/main/java/com/sindrave/caelum/api/OpenWeatherApi.java
+++ app/src/main/java/com/sindrave/caelum/api/OpenWeatherApi.java
@@ -1,11 +1,9 @@
 package com.sindrave.caelum.api;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.sindrave.caelum.domain.Forecast;
-import com.sindrave.caelum.infrastructure.Cache;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,27 +14,17 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
 
 /**
  * Created by Yanik on 15/08/2014.
  */
-// TODO: The code for caching could be put in a generic abstract super class instead of an interface...
-public class OpenWeatherApi implements WeatherApi {
+public class OpenWeatherApi extends WeatherApi {
     private static final String TAG = OpenWeatherApi.class.getName();
     private static final String OPEN_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s";
-    private static final String CACHE_DATA = "com.sindrave.caelum.cache.DATA";
-    private static final String CACHE_DATA_DATE = "CACHE_DATA_DATE";
-    private static final String CACHE_DATA_CITY = "CACHE_DATA_CITY";
-    private static final String CACHE_FILE_NAME = "forecastcache";
-    private static final int HOUR_IN_MILLIS = (1000 * 60 * 60);
     private static final int HTTP_OK_RESPONSE_CODE = 200;
-    private SharedPreferences preferences;
-    private Cache cache;
 
     public OpenWeatherApi(Context context) {
-        preferences = context.getSharedPreferences(CACHE_DATA, Context.MODE_PRIVATE);
-        cache = new Cache(context);
+        super(context);
     }
 
     private String getForecastJsonStringForCity(String city) {
@@ -81,47 +69,9 @@ public class OpenWeatherApi implements WeatherApi {
     }
 
     @Override
-    public Forecast getForecast(String city) {
-        try {
-            if (checkCacheDataIsValid(city)) {
-                Forecast forecast = (Forecast) cache.get(CACHE_FILE_NAME);
-                Log.d(TAG, "Retrieved forecast from cache: " + forecast.toString());
-                return forecast;
-            } else {
-                String forecastJson = getForecastJsonStringForCity(city);
-                JSONObject forecastDataForCity = parseToJsonObject(forecastJson);
-                Forecast forecast = OpenWeatherApiParser.parseForecastFromJson(forecastDataForCity);
-                cacheData(city, forecast);
-                Log.d(TAG, "Retrieved forecast from web: " + forecast.toString());
-                return forecast;
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Cache file does not exist or could not be created", e);
-            return null;
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, "Can't cast binary cache data to correct object", e);
-            return null;
-        } catch (Exception e) {
-            Log.e(TAG, "Error parsing the json response", e);
-            return null;
-        }
-    }
-
-    private void cacheData(String city, Forecast forecast) throws IOException {
-        preferences.edit()
-                .putString(CACHE_DATA_CITY, city)
-                .putLong(CACHE_DATA_DATE, new Date().getTime())
-                .apply();
-        cache.save(CACHE_FILE_NAME, forecast);
-    }
-
-    private boolean checkCacheDataIsValid(String newCity) {
-        String lastCity = preferences.getString(CACHE_DATA_CITY, "");
-
-        long lastCacheTime = preferences.getLong(CACHE_DATA_DATE, 0);
-        long now = new Date().getTime();
-
-        return (lastCity != null && lastCity.equals(newCity)) &&
-                (lastCacheTime != 0 && now - lastCacheTime < HOUR_IN_MILLIS);
+    protected Forecast getForecastCore(String city) {
+        String forecastJson = getForecastJsonStringForCity(city);
+        JSONObject forecastDataForCity = parseToJsonObject(forecastJson);
+        return OpenWeatherApiParser.parseForecastFromJson(forecastDataForCity);
     }
 }
--- app/src/main/java/com/sindrave/caelum/api/WeatherApi.java
+++ app/src/main/java/com/sindrave/caelum/api/WeatherApi.java
@@ -1,10 +1,75 @@
 package com.sindrave.caelum.api;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
 import com.sindrave.caelum.domain.Forecast;
+import com.sindrave.caelum.infrastructure.Cache;
+
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by Yanik on 08/10/2014.
  */
-public interface WeatherApi {
-    Forecast getForecast(String city);
+public abstract class WeatherApi {
+    private static final String TAG = WeatherApi.class.getName();
+    private static final String CACHE_DATA = "com.sindrave.caelum.cache.DATA";
+    private static final String CACHE_DATA_DATE = "CACHE_DATA_DATE";
+    private static final String CACHE_DATA_CITY = "CACHE_DATA_CITY";
+    private static final String CACHE_FILE_NAME = "forecastcache";
+    private static final int HOUR_IN_MILLIS = (1000 * 60 * 60);
+    private SharedPreferences preferences;
+    private Cache cache;
+
+    protected WeatherApi(Context context) {
+        preferences = context.getSharedPreferences(CACHE_DATA, Context.MODE_PRIVATE);
+        cache = new Cache(context);
+    }
+
+    public Forecast getForecast(String city) {
+        try {
+            if (checkCacheDataIsValid(city)) {
+                Forecast forecast = (Forecast) cache.get(CACHE_FILE_NAME);
+                Log.d(TAG, "Retrieved forecast from cache: " + forecast.toString());
+                return forecast;
+            } else {
+                Forecast forecast = getForecastCore(city);
+                cacheData(city, forecast);
+                Log.d(TAG, "Retrieved forecast from web: " + forecast.toString());
+                return forecast;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Cache file does not exist or could not be created", e);
+            return null;
+        } catch (ClassNotFoundException e) {
+            Log.e(TAG, "Can't cast binary cache data to correct object because the object " +
+                    "class definition could not be found", e);
+            return null;
+        } catch (Exception e) {
+            Log.e(TAG, "Error parsing the json response", e);
+            return null;
+        }
+    }
+
+    private void cacheData(String city, Forecast forecast) throws IOException {
+        preferences.edit()
+                .putString(CACHE_DATA_CITY, city)
+                .putLong(CACHE_DATA_DATE, new Date().getTime())
+                .apply();
+        cache.save(CACHE_FILE_NAME, forecast);
+    }
+
+    private boolean checkCacheDataIsValid(String newCity) {
+        String lastCity = preferences.getString(CACHE_DATA_CITY, "");
+
+        long lastCacheTime = preferences.getLong(CACHE_DATA_DATE, 0);
+        long now = new Date().getTime();
+
+        return (lastCity != null && lastCity.equals(newCity)) &&
+                (lastCacheTime != 0 && now - lastCacheTime < HOUR_IN_MILLIS);
+    }
+
+    protected abstract Forecast getForecastCore(String city);
 }
